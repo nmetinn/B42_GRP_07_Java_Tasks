3d22dd401a1988a743c25369c7090fa9e339cfaa
Nursemin <nurseminmetin1@gmail.com>
2026-01-05 15:59:19 -0500

    Created the project.

---
+++ src/cydeo/java/Alvaro/Week01_OddOrEven.java
@@ -0,0 +1,17 @@
+package cydeo.java.Alvaro;
+
+public class Week01_OddOrEven {
+    public static void main(String[] args) {
+        /*
+        Write a method which can identifies given number is even or odd
+         EX:
+         identify(5) -> "Odd"
+          identify(6) -> "Even"
+         */
+        oddOrEven(5);;
+    }
+
+    public static void oddOrEven(int number) {
+
+    }
+}
